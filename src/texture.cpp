da3b94b23e7870230d564a37e3bdfc60bd4c979b
Elvis Dukaj <edukaj@qubicaamf.com>
2016-04-04 10:02:56 +0200

    BUGFIX: If an unknown key was pressed application crash

--- src/texture.cpp
+++ src/texture.cpp
@@ -10,75 +10,79 @@ using namespace ogl;
 
 class impl::TextureImpl {
 public:
-    TextureImpl(const string& filename, bool flipImage = false, GLenum target = GL_TEXTURE_2D)
-        : mTarget(target)
-    {
-        mImage.load(filename, flipImage);
+	TextureImpl(const string& filename, bool flipImage = false, GLenum target = GL_TEXTURE_2D)
+		: mTarget(target)
+	{
+		mImage.load(filename, flipImage);
 
-        glGenTextures(1, &mTextureID);
+		glGenTextures(1, &mTextureID);
 
-        glEnable(mTarget);
-        bind();
+		glEnable(mTarget);
+		bind();
 
-        const auto numMipMaps = mImage.get_num_mipmaps();
-        const auto format = mImage.get_format();
+		const auto numMipMaps = mImage.get_num_mipmaps();
+		const auto format = mImage.get_format();
 
-        if (mImage.is_compressed())
-        {
-            glCompressedTexImage2D(
-                        mTarget,
-                        0,                                          // level
-                        format,
-                        mImage.get_width(), mImage.get_height(),
-                        0,                                          // border
-                        mImage.get_size(),                          // image size
-                        mImage                                      // data
-                        );
+		if (mImage.is_compressed())
+		{
+			glCompressedTexImage2D(
+						mTarget,
+						0,                                          // level
+						format,
+						mImage.get_width(), mImage.get_height(),
+						0,                                          // border
+						mImage.get_size(),                          // image size
+						mImage                                      // data
+						);
 
-            for (int i = 0; i < mImage.get_num_mipmaps(); ++i)
-            {
-                glCompressedTexImage2D(
-                            mTarget,
-                            i + 1,
-                            format,
-                            mImage.get_mipmap(i).get_width(), mImage.get_mipmap(i).get_height(),
-                            0,                                      // border
-                            mImage.get_mipmap(i).get_size(),
-                            mImage.get_mipmap(i)
-                            );
-            }
-        }
+			for (int i = 0; i < mImage.get_num_mipmaps(); ++i)
+			{
+				glCompressedTexImage2D(
+							mTarget,
+							i + 1,
+							format,
+							mImage.get_mipmap(i).get_width(), mImage.get_mipmap(i).get_height(),
+							0,                                      // border
+							mImage.get_mipmap(i).get_size(),
+							mImage.get_mipmap(i)
+							);
+			}
+		}
 
-        glTexParameteri(target, GL_TEXTURE_BASE_LEVEL, 0 );
-        glTexParameteri(target, GL_TEXTURE_MAX_LEVEL, numMipMaps);
+		glTexParameteri(target, GL_TEXTURE_BASE_LEVEL, 0 );
+		glTexParameteri(target, GL_TEXTURE_MAX_LEVEL, numMipMaps);
 
-    }
+		if(numMipMaps == 1)
+		{
+			glGenerateMipmap(target);
+		}
+	}
 
-    ~TextureImpl()
-    {
-        glDeleteTextures(1, &mTextureID);
-    }
+	~TextureImpl()
+	{
+		glDeleteTextures(1, &mTextureID);
+	}
 
-    void bind()
-    {
-        glBindTexture(mTarget, mTextureID);
-    }
+	void bind()
+	{
+		glBindTexture(mTarget, mTextureID);
+	}
 
-    void unbind()
-    {
-        glBindTexture(mTarget, 0);
-    }
+	void unbind()
+	{
+		glBindTexture(mTarget, 0);
+	}
 
 private:
-    const GLenum mTarget = GL_TEXTURE_2D;
+	const GLenum mTarget = GL_TEXTURE_2D;
 
 private:
-    nv_dds::CDDSImage mImage;
-    GLenum mTextureID;
+	nv_dds::CDDSImage mImage;
+	GLenum mTextureID;
 };
 
 Texture::Texture(const std::string& filename)
-    : mImpl{make_unique<impl::TextureImpl>(filename)}
+	: mImpl{make_unique<impl::TextureImpl>(filename)}
 {
 }
 
@@ -88,12 +92,12 @@ Texture::~Texture()
 
 void Texture::bind(GLenum textureUnit)
 {
-    glActiveTexture(textureUnit);
-    mImpl->bind();
+	glActiveTexture(textureUnit);
+	mImpl->bind();
 }
 
 void Texture::unbind(GLenum textureUnit)
 {
-    glActiveTexture(textureUnit);
-    mImpl->unbind();
+	glActiveTexture(textureUnit);
+	mImpl->unbind();
 }
